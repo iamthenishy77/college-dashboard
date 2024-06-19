12f4e4c11613ae5b039a5d9c2e2a04ecbd96f821
Paul Nischal <paulnischal.th@terralogic.com>
2024-06-19 09:24:33 +0530

    second

--- src/Admin/AdminPage.js
+++ src/Admin/AdminPage.js
@@ -1,5 +1,146 @@
+import { Button, Card, Modal, Select, Table } from 'antd';
+import React, { useState } from 'react';
 function AdminPage() {
-	return <div>This is the Admin Page</div>;
+	const [showModal, setShowModal] = useState(false);
+	const columns = [
+		{
+			title: 'Name',
+			dataIndex: 'name',
+			render: (text) => <a>{text}</a>,
+		},
+		{
+			title: 'Reg No.',
+			dataIndex: 'reg',
+		},
+		{
+			title: 'Address',
+			dataIndex: 'address',
+		},
+	];
+	const data = [
+		{
+			key: '1',
+			name: 'John Brown',
+			reg: '12JNA12A4',
+			address: 'New York No. 1 Lake Park',
+		},
+		{
+			key: '2',
+			name: 'Jim Green',
+			reg: '12JNA12A5',
+			address: 'London No. 1 Lake Park',
+		},
+		{
+			key: '3',
+			name: 'Joe Black',
+			reg: '12JNA12A6',
+			address: 'Sydney No. 1 Lake Park',
+		},
+		{
+			key: '4',
+			name: 'Dwane Red',
+			reg: '12JNA12A7',
+			address: 'Sydney No. 1 Lake Park',
+		},
+	];
+	const rowSelection = {
+		onChange: (selectedRowKeys, selectedRows) => {
+			console.log(
+				`selectedRowKeys: ${selectedRowKeys}`,
+				'selectedRows: ',
+				selectedRows
+			);
+		},
+		getCheckboxProps: (record) => ({
+			disabled: record.name === 'Disabled User',
+			// Column configuration not to be checked
+			name: record.name,
+		}),
+	};
+	return (
+		<div className='p-5'>
+			<span className='flex flex-row items-center justify-center p-3 text-2xl font-bold text-black'>
+				Admin's Portal
+			</span>
+			<Card
+				title='Add Students to Departments'
+				extra={
+					<Button className='h-10' onClick={() => setShowModal(true)}>
+						Add Student to the Department
+					</Button>
+				}
+			>
+				<div className='flex flex-row gap-5 pb-3'>
+					<Select
+						placeholder='Select Department'
+						style={{
+							width: 200,
+						}}
+						options={[
+							{
+								value: 'B.Tech',
+								label: 'B.Tech',
+							},
+							{
+								value: 'BBA',
+								label: 'BBA',
+							},
+							{
+								value: 'MBA',
+								label: 'MBA',
+							},
+							{
+								value: 'MCA',
+								label: 'MCA	',
+							},
+						]}
+					/>
+					<Select
+						placeholder='Select Department'
+						style={{
+							width: 200,
+						}}
+						options={[
+							{
+								value: 'CSE',
+								label: 'CSE',
+							},
+							{
+								value: 'ECE',
+								label: 'ECE',
+							},
+							{
+								value: 'EEE',
+								label: 'EEE',
+							},
+							{
+								value: 'Civil',
+								label: 'Civil',
+							},
+						]}
+					/>
+				</div>
+				<Table
+					rowSelection={{ rowSelection }}
+					columns={columns}
+					dataSource={data}
+				/>
+				<Button>Save</Button>
+			</Card>
+
+			<Modal
+				open={showModal}
+				onClose={() => setShowModal(false)}
+				onCancel={() => setShowModal(false)}
+			>
+				<Table
+					rowSelection={{ rowSelection }}
+					columns={columns}
+					dataSource={data}
+				/>
+			</Modal>
+		</div>
+	);
 }
 
 export default AdminPage;
--- src/Prof/Prof.js
+++ src/Prof/Prof.js
@@ -1,7 +1,212 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import {
+	Card,
+	Form,
+	Image,
+	Table,
+	Select,
+	Input,
+	Button,
+	Tag,
+	Modal,
+} from 'antd';
 function Prof() {
-	return <div>This is Lecturers Page</div>;
+	const [showModal, setShowModal] = useState(false);
+	const columns = [
+		{
+			title: 'Student Name',
+			dataIndex: 'name',
+		},
+		{
+			title: 'Reg.No',
+			dataIndex: 'internalMarks',
+			sorter: {
+				compare: (a, b) => a.chinese - b.chinese,
+				multiple: 3,
+			},
+		},
+		{
+			title: 'Department',
+			dataIndex: 'externalMarks',
+		},
+		{
+			title: 'Status',
+			dataIndex: 'total',
+			render: (status) => <Tag color='red'>{status}</Tag>,
+		},
+		{
+			title: 'Action',
+			dataIndex: 'result',
+
+			render: () => (
+				<Button onClick={() => setShowModal(true)}>Enter Marks</Button>
+			),
+		},
+	];
+	const data = [
+		{
+			key: '1',
+			name: 'John Doe',
+			internalMarks: '12SA45H12',
+			externalMarks: 'CSE',
+			total: 'Not yet entered',
+		},
+		{
+			key: '1',
+			name: 'John Doe',
+			internalMarks: '12SA45H12',
+			externalMarks: 'CSE',
+			total: 'Not yet entered',
+		},
+		{
+			key: '1',
+			name: 'John Doe',
+			internalMarks: '12SA45H12',
+			externalMarks: 'CSE',
+			total: 'Not yet entered',
+		},
+	];
+	return (
+		<div className='p-5'>
+			<span className='flex flex-row items-center justify-center p-3 text-2xl font-bold text-black'>
+				Lecturer's Portal
+			</span>
+			<div className='flex flex-col'>
+				<Card title='Lecturer Detials'>
+					<div className='flex flex-row justify-between'>
+						<Form layout='horizontal'>
+							<Form.Item label='Name' className='font-bold'>
+								<h1 className='font-medium'>Subash Chandram</h1>
+							</Form.Item>
+
+							<Form.Item label='Department' className='font-bold'>
+								<h1 className='font-medium'>CSE</h1>
+							</Form.Item>
+							<Form.Item label='Designation' className='font-bold'>
+								<h1 className='font-medium'>Asst.Proffessor</h1>
+							</Form.Item>
+						</Form>
+						<Image
+							width={200}
+							src='https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png'
+						></Image>
+					</div>
+				</Card>
+				<hr className='mt-5' />
+				<span className='pb-5 pt-5'>
+					<h1 className='text-xl font-medium pb-2'>
+						Enter Examination Results
+					</h1>
+					<div className='flex flex-row gap-6 mt-2'>
+						<Select
+							placeholder='Select Semester'
+							style={{
+								width: 200,
+							}}
+							options={[
+								{
+									value: '1st Semester',
+									label: '1st Semester',
+								},
+								{
+									value: '2nd Semester',
+									label: '2nd Semester',
+								},
+								{
+									value: '3rd Semester',
+									label: '3rd Semester',
+								},
+								{
+									value: '4th Semester',
+									label: '4th Semester',
+								},
+							]}
+						/>
+						<Select
+							placeholder='Select Department'
+							style={{
+								width: 200,
+							}}
+							options={[
+								{
+									value: 'CSE',
+									label: 'CSE',
+								},
+								{
+									value: 'Mechanical',
+									label: 'Mechanical',
+								},
+								{
+									value: 'ECE',
+									label: 'ECE',
+								},
+								{
+									value: 'EEE',
+									label: 'EEE',
+								},
+							]}
+						/>
+						<Select
+							placeholder='Select Student'
+							style={{
+								width: 200,
+							}}
+							options={[
+								{
+									value: 'John Doe',
+									label: 'John Doe',
+								},
+								{
+									value: 'John Doe',
+									label: 'John Doe',
+								},
+								{
+									value: 'John Doe',
+									label: 'John Doe',
+								},
+								{
+									value: 'John Doe',
+									label: 'John Doe',
+								},
+								{
+									value: 'John Doe',
+									label: 'John Doe',
+								},
+							]}
+						/>
+					</div>
+				</span>
+			</div>
+			<Table columns={columns} dataSource={data} />
+
+			<Modal
+				open={showModal}
+				onCancel={() => setShowModal(false)}
+				onClose={() => setShowModal(false)}
+			>
+				<div className='p-5'>
+					<Form labelCol={{ span: 5 }} title='Enter Marks'>
+						<Form.Item label='Maths'>
+							<Input placeholder='Enter Marks'></Input>
+						</Form.Item>
+						<Form.Item label='C Language'>
+							<Input placeholder='Enter Marks'></Input>
+						</Form.Item>
+						<Form.Item label='English'>
+							<Input placeholder='Enter Marks'></Input>
+						</Form.Item>
+						<Form.Item label='C Lab'>
+							<Input placeholder='Enter Marks'></Input>
+						</Form.Item>
+						<Form.Item label='Drawing Lab'>
+							<Input placeholder='Enter Marks'></Input>
+						</Form.Item>
+						<Button type='primary'>Submit</Button>
+					</Form>
+				</div>
+			</Modal>
+		</div>
+	);
 }
 
 export default Prof;
